a11aad44828d0aa6cd2b14e2bc7fd072f05b944d
Tiernan Murphy <tiernanmurphy@Jills-MacBook-Pro.local>
2026-03-30 11:27:42 -0700

    updated python version for Railway build

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
 class Topic(models.Model):
     """A topic the user is learning about"""
     text = models.CharField(max_length=200)
@@ -14,14 +13,14 @@ class Topic(models.Model):
         ordering = ['order', 'date_added']
 
     def __str__(self):
-        """Return a string representation of the model"""
+        """Return a string representation of the topic"""
         return self.text
 
 
 class Entry(models.Model):
-    """Something specific learned about a topic"""
+    """Specific entry within a topic"""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    title= models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
