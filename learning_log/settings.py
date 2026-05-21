19fedc2e2ce58b997b714106d87039e9362f0952
Tiernan Murphy <tiernanmurphy@Jills-MacBook-Pro.local>
2026-05-20 19:12:07 -0700

    added books

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = "learning_log.wsgi.application"
 
 
 # sets default database to PostgreSQL
-DATABASE_URL = os.environ.get('DATABASE_URL');
+DATABASE_URL = "postgresql://postgres:FDFZRzlrLfLZAwJFSDAHMLuvlaBqDGsF@shortline.proxy.rlwy.net:55320/railway"
 
 if DATABASE_URL:
     DATABASES = {
